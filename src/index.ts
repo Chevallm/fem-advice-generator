9b07e9a80b2a112ff346ce4bdcf5958d8fd32440
mchevallierpichon <maxime.chevallier-pichon@atinternet.com>
2022-03-08 16:28:05 +0100

    active state + api call

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-// here you put all the js you want.
+// Elements
+const randomButton = document.getElementById('randomAdvice');
+const adviceText = document.getElementById('advice');
+const adviceId = document.getElementById('advice-id');
+
+interface Advice {
+    id: number
+    advice: string
+}
+
+const url = 'https://api.adviceslip.com/advice';
+function getRandomAdvice(): Promise<Advice> {
+    return new Promise(resolve => {
+        fetch(url).then(response => {
+            response.json().then(json => {
+                resolve(json.slip)
+            })
+        })
+    });
+}
+
+function addAdvice(advice: Advice) {
+    adviceText.innerText = advice.advice;
+    adviceId.innerText = advice.id.toString();
+}
+
+getRandomAdvice().then(addAdvice);
+
+randomButton.addEventListener('click', () => {
+    getRandomAdvice().then(addAdvice);
+})
